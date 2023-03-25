69d8c28e92c14c9195433ddb5107173166427ba4
naimbro <50026338+naimbro@users.noreply.github.com>
2023-03-25 16:32:43 -0300

    Update chat.php

--- api/chat.php
+++ api/chat.php
@@ -3,7 +3,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['message'])) {
     $message = $_POST['message'];
     $apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-    $apiKey = 'your_api_key_here'; // Replace with your OpenAI API key
+    $apiKey = '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'; // Replace with your OpenAI API key
 
     $data = [
         'prompt' => $message,
